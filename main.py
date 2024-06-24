6a745abc150ae98e3d8f0033936210ff9d583695
Juan <juandiego.velez@utp.edu.co>
2024-06-24 13:54:56 -0500

    read mpu6050 with i2c

---
+++ main.py
@@ -0,0 +1,57 @@
+from machine import Pin, I2C
+import bluetooth
+from BLE import BLEUART
+from MPU6050 import MPU6050
+from time import sleep
+
+uart = None
+led = Pin(2, Pin.OUT)
+mpu = None
+
+
+def on_rx():
+    global uart
+
+    rx_data = uart.read().decode().strip()
+    uart.write("Esp32 says: " + str(rx_data) + "\n")
+    print("Esp32 says: " + str(rx_data))
+
+    if rx_data == "!B507":
+        led.on()
+    elif rx_data == "!B606":
+        led.off()
+
+
+def loop():
+    global mpu
+
+    while True:
+        gyro = mpu.read_gyro_data()
+        accel = mpu.read_accel_data()
+        temp = mpu.read_temperature()
+
+        print(gyro, accel, temp)
+
+        sleep(0.1)
+
+
+def main():
+    global uart
+    global led
+    global mpu
+
+    name = "esp32"
+    led.off()
+
+    ble = bluetooth.BLE()
+    uart = BLEUART(ble, name)
+    uart.irq(handler=on_rx)
+
+    i2c = I2C(scl=Pin(22), sda=Pin(21))
+    mpu = MPU6050(i2c)
+    mpu.wake()
+
+    loop()
+
+
+main()
